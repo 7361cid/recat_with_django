8dd576bfc69452a09c22062b1b77bb440fe46f51
Ilia <chernoffilya1997@mail.ru>
2024-10-26 15:59:29 +0700

    пустая ветка

--- shop/authentication/models.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class CustomUser (AbstractUser):
-    money = models.IntegerField(default=1000)
--- shop/product/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ProductModel, CartItemModel, ProductLikesModel, PromocodModel
-# Register your models here.
-admin.site.register(ProductModel)
-admin.site.register(PromocodModel)
-admin.site.register(CartItemModel)
-admin.site.register(ProductLikesModel)
