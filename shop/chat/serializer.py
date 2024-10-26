426325854da4a9fc1449810277fc9d1d18c8f6dc
Ilia <chernoffilya1997@mail.ru>
2024-10-26 15:33:35 +0700

    пустая ветка

--- shop/chat/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import MessageModel
-
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MessageModel
-        fields = ['chat_id', 'text', 'date_added', 'message_owner_id']
--- shop/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import MessageView, ChatView
-
-from django.urls import path
-
-urlpatterns = [
-    path('api/message', MessageView.as_view(), name="create_messages"),
-    path('api/get_chat_id', ChatView.as_view(), name="get_chat_id"),
-]
